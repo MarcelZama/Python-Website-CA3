80ed7748105bca726d6cfe009337bb732592c56d
Paul Barry <paul.james.barry@gmail.com>
2022-12-07 14:59:21 +0000

    Code refactored to use DB instead of swimclub.py code.

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import Flask, session, render_template, request
 import os
 import data_utils
 
+from records import records
+
+
 app = Flask(__name__)  # Creates a web server which can run your Flask code.and
 app.secret_key = (
     "kjhfdskjhfsdghk;odgkjsdkjsdkgsdgjdsghdjgdghdfghdfgdfghfggdf;uaghdfgagf"
@@ -49,7 +52,14 @@ def get_swimmers_names():
 @app.post("/showevents")
 def show_swimmer_files():
     name = request.form["swimmer"]
-    events = data_utils.get_swimmer_events(name)
+
+    data = data_utils.get_swimmer_data(name)  # A list of three-tuples.
+    events = [t[0] + "-" + t[1] for t in data]  # A list of events.
+    ages = list(set([t[-1] for t in data]))  # A list of unique ages.
+
+    session["swimmer"] = name
+    session["age"] = ages[0]  # The first age from the unique list.
+
     return render_template(
         "select.html",
         data=events,
@@ -59,11 +69,55 @@ def show_swimmer_files():
     )
 
 
+def convert2range(v, f_min, f_max, t_min, t_max):
+    """Given a value (v) in the range f_min-f_max, convert the value
+    to its equivalent value in the range t_min-t_max.
+
+    Based on the technique described here:
+        http://james-ramsden.com/map-a-value-from-one-number-scale-to-another-formula-and-c-code/
+    """
+    return round(t_min + (t_max - t_min) * ((v - f_min) / (f_max - f_min)), 2)
+
+
 @app.post("/showchart")
 def show_event_chart():
+    conversion = {
+        "Fly": "butterfly",
+        "Back": "backstroke",
+        "Breast": "breaststroke",
+        "Free": "freestyle",
+        "IM": "individual medley",
+    }
+
     event = request.form["event"]
-    swimclub.produce_bar_chart(event, "templates/")
-    return render_template(event.removesuffix("txt") + "html")
+
+    distance, stroke = event.split("-")
+    the_stroke = conversion[stroke]
+
+    the_event = f"{distance} {the_stroke}"
+
+    lcmen = records["LCMen"][the_event]
+    lcwomen = records["LCWomen"][the_event]
+    scmen = records["SCMen"][the_event]
+    scwomen = records["SCWomen"][the_event]
+
+    swimmer = session["swimmer"]
+    age = session["age"]
+
+    average_str, times, converts = data_utils.get_chart_data(swimmer, age, event)
+    the_converts = [convert2range(c, 0, max(converts), 0, 350) for c in converts]
+
+    chart_data = list(zip(times, the_converts))
+
+    the_title = f"{swimmer} (Under {age}) {event}"
+
+    return render_template(
+        "chart.html",
+        title=the_title,
+        data=chart_data,
+        average=average_str,
+        worlds=[lcmen, lcwomen, scmen, scwomen],
+    )
 
 
 if __name__ == "__main__":
--- data_utils.py
+++ data_utils.py
@@ -1,3 +1,5 @@
+import statistics
+
 import DBcm
 
 config = {
@@ -17,9 +19,9 @@ def get_swimmers_list():
     return names
 
 
-def get_swimmer_events(name):
+def get_swimmer_data(name):
     SQL = """ 
-        select distinct strokes.distance, strokes.stroke
+        select distinct strokes.distance, strokes.stroke, swimmers.age
         from swimmers, strokes, times
         where times.swimmer_id = swimmers.id and
         times.stroke_id = strokes.id and
@@ -28,5 +30,45 @@ def get_swimmer_events(name):
     with DBcm.UseDatabase(config) as db:
         db.execute(SQL, (name,))
         results = db.fetchall()  # a list of tuples.
-    events = [t[0] + "-" + t[1] for t in results]  # a list of swimming events.
-    return events
+    ## events = [t[0] + "-" + t[1] for t in results]  # a list of swimming events.
+    return results
+
+
+def get_chart_data(name, age, event):
+    distance, stroke = event.split("-")
+    SQL = """
+        select swimmers.name, swimmers.age, times.time, strokes.distance, strokes.stroke, times.ts
+        from swimmers, strokes, times
+        where (swimmers.name = %s and swimmers.age = %s) and
+        (strokes.distance = %s and strokes.stroke = %s) and 
+        swimmers.id = times.swimmer_id and
+        strokes.id = times.stroke_id
+    """
+    with DBcm.UseDatabase(config) as db:
+        db.execute(
+            SQL,
+            (
+                name,
+                age,
+                distance,
+                stroke,
+            ),
+        )
+        results = db.fetchall()
+    times = [item[2] for item in results]
+    converts = []  # This is an empty list.
+    for t in times:
+        if ":" in t:
+            mins, the_rest = t.split(":")
+            secs, hundredths = the_rest.split(".")  # Darius.
+        else:
+            mins = 0
+            secs, hundredths = t.split(".")  # Dave.
+        converts.append((int(mins) * 60 * 100) + (int(secs) * 100) + int(hundredths))
+    average = statistics.mean(converts)
+    mins = int((average // 60) // 100)
+    remainder = average - (mins * 60 * 100)
+    secs = int((remainder // 100))
+    hundredths = int(round(remainder - (secs * 100), 0))
+    average_str = f"{mins}:{secs}.{hundredths}"
+    return average_str, times, converts
